aca623c1d50b63221424ff82a812923a7bec9774
verdantgreeny <verdantgreeny@gmail.com>
2025-02-20 19:40:15 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/shared/PublicRoutes.jsx
@@ -0,0 +1,10 @@
+// PublicRoutes : 로그인이 필요없는 페이지에 접근할 수 있도록 하는 컴포넌트
+// 로그인이 되어있는 사용자는 mypage로 리다이렉트
+
+import React from "react";
+
+const PublicRoutes = () => {
+  return <div>PublicRoutes</div>;
+};
+
+export default PublicRoutes;
