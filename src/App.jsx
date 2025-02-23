21c42f827fb991619dac6b5d2b2ab8b4dce229a7
verdantgreeny <verdantgreeny@gmail.com>
2025-02-23 19:46:01 +0900

    refactor: 필요없는 주석 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,15 @@ function App() {
   return (
     <AuthProvider>
       <ToastContainer
-        position="top-right" // 알람 위치 지정
-        autoClose={2000} // 자동 off 시간
-        hideProgressBar={false} // 진행시간바 숨김
-        closeOnClick // 클릭으로 알람 닫기
-        rtl={false} // 알림 좌우 반전
-        pauseOnFocusLoss // 화면을 벗어나면 알람 정지
-        draggable // 드래그 가능
-        pauseOnHover // 마우스를 올리면 알람 정지
-        theme="light"
-        // limit={1} // 알람 개수 제한
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHove
+        theme="dark"
       />
       <Router />
     </AuthProvider>
