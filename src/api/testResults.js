d4cd22be6c2b0f8c5e1650f5c6982e2d6ef67091
verdantgreeny <verdantgreeny@gmail.com>
2025-02-24 16:07:25 +0900

    chore: json-server URL 변경

--- src/api/testResults.js
+++ src/api/testResults.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/testResults";
+const API_URL = "https://buttercup-axiomatic-currant.glitch.me";
 
 //테스트 결과들 가져오기
 export const getTestResults = async () => {
