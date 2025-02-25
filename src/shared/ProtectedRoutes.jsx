c76ce6fcb5eb617f7a8e357d8843e491c3f7bfc8
verdantgreeny <verdantgreeny@gmail.com>
2025-02-25 11:46:55 +0900

    Cleanup: 프로텍트라우터 알람 삭제

--- src/shared/ProtectedRoutes.jsx
+++ src/shared/ProtectedRoutes.jsx
@@ -1,26 +1,22 @@
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { ROUTES } from "../constants/routes";
-import { toast } from "react-toastify";
 import useAuthStore from "../zustand/bearsStore";
 
 const ProtectedRoutes = ({ element, isLogin }) => {
   const { isAuthenticated } = useAuthStore();
   const { HOME, LOGIN } = ROUTES;
   const navigate = useNavigate();
-  const hasAlerted = useRef(false);
 
   useEffect(() => {
-    if (!isLogin && !isAuthenticated && !hasAlerted.current) {
+    if (!isLogin && !isAuthenticated) {
       // 로그인해야 하는 페이지인데 로그인X
-      hasAlerted.current = true;
       navigate(LOGIN);
-    } else if (isLogin && isAuthenticated && !hasAlerted.current) {
+    } else if (isLogin && isAuthenticated) {
       // 로그인하지 않아야 하는 페이지인데 로그인O
-      hasAlerted.current = true;
       navigate(HOME);
     }
-  }, [navigate, isLogin, isAuthenticated]);
+  }, [isLogin, isAuthenticated]);
 
   return element;
 };
